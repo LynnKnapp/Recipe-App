d688a96a4c43bfec0574409600f7dceff425e7b4
Lynn Knapp <ebonyeek@gmail.com>
2019-12-09 21:54:58 -0500

    css changes

--- client/src/components/Auth/AuthForm.js
+++ client/src/components/Auth/AuthForm.js
@@ -3,21 +3,23 @@ import React from 'react'
 const AuthForm = props =>{
     const {username, password, handleChange, handleSubmit, btnText} = props
         return(
-            <form className ='form' onSubmit ={handleSubmit}>
-                <input 
-                type='text' 
-                name='username' 
-                value={username}
-                onChange={handleChange} 
-                placeholder='Username'/>
-                <input className = 'input'
-                type='text'
-                 name='password' 
-                 value={password} 
-                 onChange={handleChange}
-                 placeholder='Password'/>
-                <button className = 'btn'>{btnText}</button>
-            </form>
+            <div className= 'auth-form'>
+                <form className ='form' onSubmit ={handleSubmit}>
+                    <input 
+                    type='text' 
+                    name='username' 
+                    value={username}
+                    onChange={handleChange} 
+                    placeholder='Username'/>
+                    <input className = 'input'
+                    type='text'
+                    name='password' 
+                    value={password} 
+                    onChange={handleChange}
+                    placeholder='Password'/>
+                    <button className = 'btn'>{btnText}</button>
+                </form>
+            </div>
         )
 }
 
